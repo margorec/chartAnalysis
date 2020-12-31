06f2a3ff437b49ec9daf9243b7c1e3fbce2f4530
Marcin Gorecki <marcin_gorecki@epam.com>
2020-12-31 15:18:48 +0100

    Adding keeping previous symbols in the http session and UI for display it.

--- src/main/java/com/marcingorecki/ChartAnalysis/controller/ChartController.java
+++ src/main/java/com/marcingorecki/ChartAnalysis/controller/ChartController.java
@@ -6,8 +6,10 @@ import com.marcingorecki.ChartAnalysis.service.SessionService;
 import com.marcingorecki.ChartAnalysis.service.StooqParser;
 import com.marcingorecki.ChartAnalysis.service.TimeService;
 import com.marcingorecki.ChartAnalysis.service.TimeseriesProcessor;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -30,8 +32,11 @@ public class ChartController {
     private final TimeService timeService;
     private final SessionService sessionService;
 
+    private final int SYMBOLS_TO_SHOW = 5;
+
     @Autowired
-    public ChartController(StooqParser parser, TimeseriesProcessor timeseriesProcessor, TimeService timeService, SessionService sessionService) {
+    public ChartController(StooqParser parser, TimeseriesProcessor timeseriesProcessor, TimeService timeService,
+        SessionService sessionService) {
         this.timeseriesService = timeseriesProcessor;
         this.parser = parser;
         this.timeService = timeService;
@@ -39,29 +44,41 @@ public class ChartController {
     }
 
     @RequestMapping(value = CHART_VIEW_NAME, method = RequestMethod.GET)
-    public String selectAssetSybmol(Map<String, Object> model,
-                                    @RequestParam(required = false, value = "symbol", defaultValue = DEFAULT_SYMBOL) Optional<String> symbol,
-                                    @RequestParam(required = false, value = "period", defaultValue = DEFAULT_PERIOD) Optional<String> period,
-                                    final HttpSession session) {
-        symbol.ifPresentOrElse( s -> sessionService.storeSymbol(s, session), ()-> System.out.println("No symbol to store"));
+    public String selectAssetSymbol(Map<String, Object> model,
+        @RequestParam(required = false, value = "symbol", defaultValue = DEFAULT_SYMBOL) Optional<String> symbol,
+        @RequestParam(required = false, value = "period", defaultValue = DEFAULT_PERIOD) Optional<String> period,
+        final HttpSession session) {
+
+        symbol.ifPresent(s -> sessionService.storeSymbol(s, session));
+
+        model.put("symbols", preparePreviousSymbols(symbol, session));
         model.put("asset", new Asset(symbol.get().toUpperCase()));
-        model.put("chartData", prepareData(symbol.get().toUpperCase(), period.get()));
+        model.put("chartData", prepareChartData(symbol.get().toUpperCase(), period.get()));
         sessionService.getPreviousSymbols(session);
         return CHART_VIEW_NAME;
     }
 
-    private Map<String, Triplet> prepareData(String assetSymbol, String period) {
+    private Set<String> preparePreviousSymbols(Optional<String> symbol, HttpSession session) {
+        Set<String> previousSymbols = new HashSet<>(sessionService.getPreviousSymbols(session));
+        System.out.println(previousSymbols);
+        symbol.ifPresent(previousSymbols::remove);
+        return previousSymbols;
+    }
+
+    private Map<String, Triplet> prepareChartData(String assetSymbol, String period) {
         Map<String, Double> data = parser.downloadAndProcess(assetSymbol);
-        Map<String, Double> shortMovingAvg = timeseriesService.getMovingAverage(data, timeseriesService.shortAvgRange(data.size()));
-        Map<String, Double> longMovingAvg = timeseriesService.getMovingAverage(data, timeseriesService.longAvgRange(data.size()));
+        Map<String, Double> shortMovingAvg = timeseriesService
+            .getMovingAverage(data, timeseriesService.shortAvgRange(data.size()));
+        Map<String, Double> longMovingAvg = timeseriesService
+            .getMovingAverage(data, timeseriesService.longAvgRange(data.size()));
 
         TreeMap<String, Triplet> result = data.keySet()
-                .stream()
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        k -> new Triplet(data.get(k), shortMovingAvg.get(k), longMovingAvg.get(k)),
-                        (a, b) -> a, TreeMap::new
-                ));
+            .stream()
+            .collect(Collectors.toMap(
+                Function.identity(),
+                k -> new Triplet(data.get(k), shortMovingAvg.get(k), longMovingAvg.get(k)),
+                (a, b) -> a, TreeMap::new
+            ));
 
         return result.tailMap(timeService.getBackwardDate(period));
     }
---
+++ src/main/java/com/marcingorecki/ChartAnalysis/service/SessionService.java
@@ -0,0 +1,34 @@
+package com.marcingorecki.ChartAnalysis.service;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionService {
+
+    private static final String PREVIOUS_SYMBOLS = "previousSymbols";
+
+    public void storeSymbol(String symbol, HttpSession session) {
+        Set<String> previousSymbols = createOrFetchSymbols(session);
+
+        previousSymbols.add(symbol.toUpperCase());
+        session.setAttribute(PREVIOUS_SYMBOLS, previousSymbols);
+    }
+
+    public Set<String> getPreviousSymbols(HttpSession session) {
+        return (Set<String>) session.getAttribute(PREVIOUS_SYMBOLS);
+    }
+
+    private Set<String> createOrFetchSymbols(HttpSession session) {
+        Set<String> previousSymbols = (Set<String>) session.getAttribute(PREVIOUS_SYMBOLS);
+
+        if (previousSymbols != null) {
+            return previousSymbols;
+        }
+        return new HashSet<>();
+    }
+
+}
